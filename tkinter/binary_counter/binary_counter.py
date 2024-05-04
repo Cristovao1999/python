de1c5b4ccfe1f051162f6a984272df2770433884
@dalcripeto <73932019+Cristovao1999@users.noreply.github.com>
2024-05-04 06:48:51 +0200

    Update binary_counter.py

--- tkinter/binary_counter/binary_counter.py
+++ tkinter/binary_counter/binary_counter.py
@@ -2,42 +2,41 @@ from tkinter import *
 from tkinter import ttk
 import time
 from threading import Thread
-def binary_counter():
-    
+
+def binary_counter():    
     root = Tk()
     root.title('Binary counter')
     f = ttk.Frame(root)
     f.grid()
 
     def count_bits():
-        bits = [
-            '0000', # -- 0
-            '0001', # -- 1
-            '0010', # -- 2
-            '0011', # -- 3
-            '0100', # -- 4
-            '0101', # -- 5
-            '0110', # -- 6
-            '0111', # -- 7
-            '1000', # -- 8
-            '1001', # -- 9
-            '1010', # -- 10
-            '1011', # -- 11
-            '1100', # -- 12
-            '1101', # -- 13
-            '1110', # -- 14
-            '1111', # -- 15
-        ]
+        # bits = [
+        #     '0000', # -- 0
+        #     '0001', # -- 1
+        #     '0010', # -- 2
+        #     '0011', # -- 3
+        #     '0100', # -- 4
+        #     '0101', # -- 5
+        #     '0110', # -- 6
+        #     '0111', # -- 7
+        #     '1000', # -- 8
+        #     '1001', # -- 9
+        #     '1010', # -- 10
+        #     '1011', # -- 11
+        #     '1100', # -- 12
+        #     '1101', # -- 13
+        #     '1110', # -- 14
+        #     '1111', # -- 15
+        # ]
         x = 0
         while x < 100:
-            for bit in bits:
+            for bit in range(16):
+                bits = f'{bit:04b}'
                 for i, square in enumerate('abcd'):
-                    squares.itemconfig(my_bits[square], fill='dark cyan' if bit[i] == '1' else 'white')
+                    squares.itemconfig(my_bits[square], fill='dark cyan' if bits[i] == '1' else 'white')
                 time.sleep(0.5)
                 root.update()
             x+=1
-
-
 
     squares = Canvas(f, width=500,  height=200)
     my_bits = {}
